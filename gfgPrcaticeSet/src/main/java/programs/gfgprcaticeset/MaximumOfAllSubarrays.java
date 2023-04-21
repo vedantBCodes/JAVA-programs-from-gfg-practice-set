a16c474cb4622827d79dfcf4ff50d5fe26afce79
vedantBCodes <bichkundevedant97@gmail.com>
2023-04-21 15:23:09 -0700

    new program added

---
+++ gfgPrcaticeSet/src/main/java/programs/gfgprcaticeset/MaximumOfAllSubarrays.java
@@ -0,0 +1,43 @@
+//Given an array arr[] of size "size" and a size of subarray equals to 3.
+//Find the maximum for each and every contiguous subarray of size 3.
+package programs.gfgprcaticeset;
+
+import static java.lang.System.exit;
+import java.util.Scanner;
+public class MaximumOfAllSubarrays {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.print("Enter the size of array:");
+        int size=obj.nextByte();
+        int arr[]=new int[size];
+        System.out.print("Enter any "+size+" numbers:");
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=obj.nextInt();
+        }
+        int x=3;
+       /*int size=8;
+       int arr[]={1,2,3,4,5,6,7,8};
+       int x=3;*/
+       int max;
+        System.out.print("Maximum numbers in a contigous subarrays of size 3 are:");
+        for(int i=0;i<size;i++)
+        {
+            max=arr[i];
+            for(int j=i;j<=(x-1);j++)
+            {
+                if(arr[j]>max)
+                {
+                    max=arr[j];
+                }
+            }
+            System.out.print(max+" ");
+            if(x==(size))
+            {
+                exit(0);
+            }
+            x++;
+        }
+    }
+    
+}
